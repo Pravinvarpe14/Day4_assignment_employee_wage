776cd1de45e340ae278b843e628f39259edec8ec
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-31 20:03:20 +0530

    added employee present or absent programme

--- src/Employee_wage.java
+++ src/Employee_wage.java
@@ -8,6 +8,28 @@ public class Employee_wage {
 		 */
 		System.out.println("Welcome to employee wage computation programme");
 
+		/*
+		 * taking variable
+		 */
+		int isPresent = 1;
+		/*
+		 * using math.floor 
+		 */
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		
+		/*
+		 * verifying by if else statement
+		 */
+		
+		if (empCheck == isPresent) {
+		
+			
+			System.out.println("Employee is present...");
+		} else {
+			
+			System.out.println("Employee is absent...");
+		}
+		
 	}
 
 }
