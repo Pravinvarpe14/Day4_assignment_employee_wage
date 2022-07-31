ccf65abe0210bdd31f3abfaf4b441814be36db5a
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-31 19:59:43 +0530

    added welcome message programme

---
+++ src/Employee_wage.java
@@ -0,0 +1,13 @@
+
+public class Employee_wage {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * welcome message
+		 */
+		System.out.println("Welcome to employee wage computation programme");
+
+	}
+
+}
